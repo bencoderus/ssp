3384d9c3e19916dc209a2008857c49861ccfad20
Benjamin Iduwe <bencoderus@gmail.com>
2021-10-17 16:17:05 +0100

    Docker setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    {{--
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ if [[ $ROLE == "app" ]]
 then
 echo "running as app"
 php artisan config:clear
-php artisan migrate --force --no-interaction
 php artisan clear-compiled
 php artisan auth:clear-resets
 php artisan optimize:clear
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
+php artisan migrate --force --no-interaction
 php artisan db:seed
 find * -type f -exec chmod 644 {} \;
 find * -type d -exec chmod 755 {} \;
